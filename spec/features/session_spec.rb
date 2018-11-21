2ef2355a4f3d4777d580d581e78abcf1308420a6
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-11-21 05:56:56 +0000

    automatically backed up by learn

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -40,7 +40,7 @@ RSpec.feature "Sessions", type: :feature do
       fill_in "name", with: ""
       click_button "login"
 
-      expect(response)
+      expect(response).to redirect_to '/login'
     end
   end
 end
