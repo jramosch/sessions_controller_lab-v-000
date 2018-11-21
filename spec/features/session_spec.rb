c8ae5c2cb93f336e581fdae4cdcfe3eb7d48462b
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-11-21 05:30:14 +0000

    automatically backed up by learn

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Sessions", type: :feature do
   describe "homepage", type: :view do
     before do
-      session.clear
+      session.delete :name
     end
 
     it "shows login link if user is not logged in" do
